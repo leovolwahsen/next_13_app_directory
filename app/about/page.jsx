f7fedf649ea7f9b573d221b76cede15f200bf764
Leo Volwahsen <volwahsenl@gmail.com>
2023-11-22 16:16:06 +0100

    App directory in use, foulders are named to what it is and inside page.jsx are always named page. In Homepage we use Link i.e. <Link href='/about/team'>Team</Link> this is a directory inanother directory of the URL

---
+++ app/about/page.jsx
@@ -0,0 +1,18 @@
+
+const AboutPage = () => {
+  return <div>
+    <h1>About Leo Multimedia</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repudiandae pariatur officia praesentium aliquam eligendi harum tenetur laborum est neque nesciunt. Est explicabo animi repellat quia recusandae quis neque, fuga ex.
+    Placeat temporibus harum possimus ad. Nisi nihil sapiente minus explicabo voluptas nostrum voluptatibus repellat velit aspernatur, perferendis excepturi placeat mollitia veritatis. Voluptate temporibus quos repellendus, fuga nemo minus unde magni?
+    Commodi itaque fuga aliquid quidem facilis, nulla et officia voluptate beatae vitae veritatis, consequuntur nam, eos iusto maiores earum nostrum error magni? Sequi dolore necessitatibus magnam, quae atque quo aut.
+    Mollitia natus dolore distinctio saepe recusandae culpa iusto quisquam harum. Voluptatem, eligendi est repellat laboriosam iusto tenetur vel? Nam, iure? Expedita, quibusdam numquam exercitationem eligendi ratione magni explicabo molestiae assumenda?
+    Voluptas tenetur officiis enim quibusdam voluptatum saepe dolores aperiam vero aspernatur non eum culpa, ducimus corrupti ad recusandae nemo. Molestiae dicta est eveniet magnam labore aspernatur sunt illo quisquam harum.
+    Voluptates in quod ut exercitationem quis nihil facilis consequuntur, suscipit velit, reprehenderit omnis doloremque ducimus modi maxime vel dicta provident nobis soluta quia minus repellendus sapiente! Dolores numquam neque repellendus?
+    Saepe, quis? Est consequuntur obcaecati inventore porro eveniet repudiandae nesciunt facilis, atque nam nisi soluta vero libero, officiis modi ipsum, non laudantium sint quam doloremque consectetur. Recusandae dignissimos tempora temporibus.
+    Recusandae voluptate consequuntur qui, quia nesciunt nihil magnam commodi atque obcaecati voluptatem blanditiis incidunt a omnis, libero iure temporibus, hic officiis perferendis et perspiciatis. Assumenda vitae at vel facere nisi.
+    In impedit natus quas, optio voluptatibus cumque eum dignissimos quaerat corporis id earum numquam magnam est rerum repellat? Sit hic et velit perferendis quae? Adipisci temporibus dignissimos alias. Quibusdam, sequi!
+    Cumque ea repudiandae, et atque reprehenderit cupiditate suscipit maxime, voluptatum nihil quae inventore quibusdam minus unde obcaecati dicta iure voluptatem asperiores quis ex tempore. Voluptates dignissimos illum ullam deleniti voluptatem!</p>
+  </div>;
+}
+
+export default AboutPage;
---
+++ app/about/team/page.jsx
@@ -0,0 +1,15 @@
+
+const TeamPage = () => {
+  return (
+    <div>
+      <h1>Our Team</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat vel fuga numquam, dolorem libero omnis praesentium, sequi ab architecto doloribus expedita tempora vitae voluptatem rem iure! Reiciendis veritatis numquam consectetur.
+      Explicabo quia laborum, quas odio a ratione velit, sunt commodi atque enim id cupiditate aliquid optio nulla beatae voluptate veritatis repudiandae doloremque voluptates similique repellat in! Quisquam corporis voluptas mollitia!
+      Iste soluta aut illo vero quo, quidem iure a quasi quibusdam facere cumque nihil numquam facilis blanditiis maiores fuga deleniti, accusantium aliquid vitae. Illum amet nemo quo nesciunt ratione omnis!
+      Quaerat nostrum, debitis tempore commodi sed soluta. Alias magnam nemo, blanditiis eius ullam veritatis accusamus error voluptatibus eaque rerum corrupti laboriosam eligendi perspiciatis dolorem neque enim facilis eos aspernatur natus?
+      Rerum impedit aliquid dolor veniam ullam omnis blanditiis illo, dolore nemo ab maiores accusamus excepturi, neque aliquam quod molestiae. Minus, voluptatum nihil autem commodi libero enim quos itaque. Dolores, non?</p>
+    </div>
+  )
+}
+
+export default TeamPage
